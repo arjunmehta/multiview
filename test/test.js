b26d583edcfd1dea9b4bee1b312ed0feb08acbd3
Arjun Mehta <arjunmeht@gmail.com>
2015-02-19 14:23:49 -0400

    Adding basic test.

--- test/test.js
+++ test/test.js
@@ -1,25 +1,18 @@
-var timer = parseInt(Math.random()*1000, 10);
-// var timer = 50;
+var MultiView = require('../main');
 
-var count = 0;
+exports['Exported Properly'] = function(test) {
 
-if(process.stdout.isTTY){
-  console.log('not redirected');
-}
-else {
-  console.log('\033[31mredirected ' + new Date().getMilliseconds() + "\033[0m");
-  console.log('\033[32mTest ' + new Date().getMilliseconds() + "\033[0m");
-}
+    var mv = new MultiView();
 
-// process.exit(0);
+    test.expect(3);
 
-setInterval(function(){
-  if(count > 100){
-    process.exit(1);
-  }
-  process.stdout.write("TESTING PIPE" + process.pid +" " + (count++) + " " + Math.random()*298732487847 + "\n"); 
-},timer);
+    test.equal(typeof mv, 'object');
+    test.equal(typeof mv.spawn, 'function');
+    test.equal(typeof mv.stream, 'function');
 
-process.stdout.on('error', function(err) {
-    process.exit(0);
-});
+    test.done();
+};
+
+exports['tearDown'] = function(done) {
+    done();
+};
